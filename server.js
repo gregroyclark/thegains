2431844c544bf08f060d149652062d091fa7e4a6
peacefrog <gregroyclark@gmail.com>
2020-06-04 10:21:30 -0500

    connects mongoose.js

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose =  require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+//require models? Does this app need models?
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI ||  "mongodb://localhose/populatedb", { useNewUrlParser: true });
