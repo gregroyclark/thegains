682a835548369231f0df1c07dc6462152a3ab287
peacefrog <gregroyclark@gmail.com>
2020-06-05 09:44:42 -0500

    adds "useUnifiedTopology: true" to mongoose connection

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI ||  "mongodb://localhose/populatedb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI ||  "mongodb://localhose/populatedb", { useNewUrlParser: true, useUnifiedTopology: true });
 
 // routes?
 //app.use(require("./routes/api.js"));
